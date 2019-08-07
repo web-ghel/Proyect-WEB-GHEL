55ec2c07700b16366fa71dbc516267a5f0fadb2b
eduatigs <eduatigs96@gmail.com>
2019-08-07 11:18:36 -0500

    ORM de la base de datos : Proyecto

---
+++ sequelize/db.js
@@ -0,0 +1,82 @@
+//Instalar sequelize
+//Instalar mysql2
+
+const Sequelize = require('sequelize');
+const Model = Sequelize.Model;
+
+const sequelize = new Sequelize('Proyecto','root','root',{
+	host: 'localhost',
+	dialect: 'mysql'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conexion exitosa');
+  })
+  .catch(err => {
+    console.error('Erro al abrir la base de datos', err);
+  });
+
+class Usuario extends Model{}
+Usuario.init({
+	idUser: { type: Sequelize.STRING, primaryKey: true },
+	nombre: Sequelize.STRING,
+	apellido: Sequelize.STRING,
+	email: Sequelize.STRING,
+	telefono: Sequelize.STRING,
+	direccion: Sequelize.STRING,
+	esvendedor: Sequelize.BOOLEAN,
+	escliente: Sequelize.BOOLEAN,
+	esadministrador: Sequelize.BOOLEAN
+},{ sequelize, modelName: 'usuario'});
+
+class Producto extends Model{}
+Producto.init({
+	idProduc: { type: Sequelize.INTEGER, primaryKey: true },
+	nombre: Sequelize.STRING,
+	precio: Sequelize.DECIMAL,
+	color: Sequelize.STRING,
+	tamano: Sequelize.STRING,
+	beneficio: Sequelize.STRING,
+	vendedor: { type:Sequelize.STRING, references: {
+		model: Usuario, key: 'idUser'
+	}}
+},{ sequelize, modelName: 'producto'});
+
+class Tienda extends Model{}
+Producto.init({
+	idLocal: { type: Sequelize.INTEGER, primaryKey: true },
+	nombre: Sequelize.STRING,
+	telefono: Sequelize.STRING,
+	direccion: Sequelize.STRING,
+	vendedor: { type:Sequelize.STRING, references: {
+		model: Usuario, key: 'idUser'
+	}}
+},{ sequelize, modelName: 'tienda'});
+
+class Mensaje_Usuario extends Model{}
+Producto.init({
+	idMU: { type: Sequelize.INTEGER, primaryKey: true },
+	contenido: Sequelize.TEXT,
+	vendedor: { type:Sequelize.STRING, references: {
+		model: Usuario, key: 'idUser'
+	}},
+	cliente: { type:Sequelize.STRING, references: {
+		model: Usuario, key: 'idUser'
+	}}
+},{ sequelize, modelName: 'mensajeusuario'});
+
+class Mensaje_Producto extends Model{}
+Producto.init({
+	idMU: { type: Sequelize.INTEGER, primaryKey: true },
+	contenido: Sequelize.TEXT,
+	producto: { type:Sequelize.INTEGER, references: {
+		model: Producto, key: 'idProduc'
+	}},
+	cliente: { type:Sequelize.STRING, references: {
+		model: Usuario, key: 'idUser'
+	}}
+},{ sequelize, modelName: 'mensajeproducto'});
+
+module.exports = {Usuario, Producto, Tienda, Mensaje_Producto, Mensaje_Usuario};
